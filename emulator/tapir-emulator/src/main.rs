9a1c947d721f99302126f21ec7e8748b5ffc7780
Corwin <corwin@kuiper.dev>
2023-11-05 19:28:47 +0000

    add scaling options

--- emulator/tapir-emulator/src/main.rs
+++ emulator/tapir-emulator/src/main.rs
@@ -7,6 +7,7 @@ use sdl2::{
     event::Event,
     keyboard::{Keycode, Scancode},
     pixels::PixelFormatEnum,
+    rect::{Point, Rect},
 };
 
 use crate::resampler::{calculate_dynamic_rate_ratio, Resampler};
@@ -14,6 +15,56 @@ use crate::resampler::{calculate_dynamic_rate_ratio, Resampler};
 mod resampler;
 
 const GBA_FRAMES_PER_SECOND: f64 = 59.727500569606;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum ScalingOptions {
+    Stretch,
+    Letterbox,
+    PixelPerfect,
+}
+
+const GBA_WIDTH: u32 = 240;
+const GBA_HEIGHT: u32 = 160;
+
+impl ScalingOptions {
+    fn output_rectangle(self, output_size: (u32, u32)) -> Rect {
+        let (width, height) = output_size;
+
+        match self {
+            ScalingOptions::Stretch => Rect::new(0, 0, width, height),
+            ScalingOptions::Letterbox => {
+                let x_divisor = width as f64 / GBA_WIDTH as f64;
+                let y_divisor = height as f64 / GBA_HEIGHT as f64;
+
+                if y_divisor < x_divisor {
+                    // use height as baseline
+                    let out_width = GBA_WIDTH as f64 * y_divisor;
+                    let out_width = out_width.round() as u32;
+
+                    Rect::new(((width - out_width) / 2) as i32, 0, out_width, height)
+                } else {
+                    // use height as baseline
+                    let out_height = GBA_HEIGHT as f64 * x_divisor;
+                    let out_height = out_height.round() as u32;
+
+                    Rect::new(0, ((height - out_height) / 2) as i32, width, out_height)
+                }
+            }
+            ScalingOptions::PixelPerfect => {
+                let x_divisor = width / GBA_WIDTH;
+                let y_divisor = height / GBA_HEIGHT;
+
+                let scale_factor = x_divisor.min(y_divisor);
+
+                Rect::from_center(
+                    Point::new((width / 2) as i32, (height / 2) as i32),
+                    GBA_WIDTH * scale_factor,
+                    GBA_HEIGHT * scale_factor,
+                )
+            }
+        }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     let sdl_context = sdl2::init().unwrap();
@@ -23,8 +74,6 @@ fn main() -> anyhow::Result<()> {
         mgba::MCore::new().ok_or_else(|| anyhow::anyhow!("Failed to initialise mgba core"))?;
     mgba_core.load_rom(mgba::MemoryBacked::new(rom_data));
 
-    let (width, height) = (240, 160);
-
     let video_subsystem = sdl_context
         .video()
         .map_err(|e| anyhow::anyhow!("Failed to initialise video subsystem {e}"))?;
@@ -33,16 +82,20 @@ fn main() -> anyhow::Result<()> {
         .map_err(|e| anyhow::anyhow!("Failed to initialise audio subsystem {e}"))?;
 
     let window = video_subsystem
-        .window("Tapir emulator", width * 3, height * 3)
+        .window("Tapir emulator", GBA_WIDTH * 3, GBA_HEIGHT * 3)
         .position_centered()
+        .resizable()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().accelerated().present_vsync().build()?;
 
     let texture_creator = canvas.texture_creator();
-    let mut texture =
-        texture_creator.create_texture_streaming(PixelFormatEnum::ABGR8888, width, height)?;
+    let mut texture = texture_creator.create_texture_streaming(
+        PixelFormatEnum::ABGR8888,
+        GBA_WIDTH,
+        GBA_HEIGHT,
+    )?;
 
     let mut event_pump = sdl_context
         .event_pump()
@@ -80,6 +133,8 @@ fn main() -> anyhow::Result<()> {
     let audio_sample_rate = audio_system.spec().freq as f64;
 
     mgba_core.set_audio_frequency(audio_sample_rate);
+
+    let scaling_option = ScalingOptions::PixelPerfect;
 
     let mut keys = 0;
 
@@ -155,8 +210,16 @@ fn main() -> anyhow::Result<()> {
             })
             .map_err(|e| anyhow::anyhow!("Failed to copy mgba texture {e}"))?;
 
+        let canvas_size = canvas
+            .output_size()
+            .map_err(|e| anyhow::anyhow!("Failed to get size of canvas {e}"))?;
+
+        let output_rectangle = scaling_option.output_rectangle(canvas_size);
+
+        canvas.clear();
+
         canvas
-            .copy(&texture, None, None)
+            .copy(&texture, None, output_rectangle)
             .map_err(|e| anyhow::anyhow!("Failed to copy texture {e}"))?;
         canvas.present();
     }
