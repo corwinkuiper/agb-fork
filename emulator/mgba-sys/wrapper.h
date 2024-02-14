57966cda7318eebca11e2c61bbbb7549a29eee2e
Corwin <corwin@kuiper.dev>
2024-02-14 19:47:32 +0000

    expose mgba internals

--- emulator/mgba-sys/wrapper.h
+++ emulator/mgba-sys/wrapper.h
@@ -1,6 +1,8 @@
 #include "mgba/include/mgba-util/vfs.h"
 #include "mgba/include/mgba/core/blip_buf.h"
 #include "mgba/include/mgba/core/core.h"
+#include "mgba/include/mgba/core/cpu.h"
 #include "mgba/include/mgba/core/log.h"
 #include "mgba/include/mgba/core/timing.h"
 #include "mgba/include/mgba/gba/core.h"
+#include "mgba/include/mgba/internal/arm/arm.h"
